2ae7dd08ec18060663ca77a92ece5e55c76dee2c
Peter Kahl <peter.kahl@colossalmind.com>
2017-05-14 08:28:27 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# Gender-Universal
+# Gender Universal
+
 Detects gender from person's name, capable to analyse strings in these scripts: Latin, Cyrillic, Hanzi (Kanji).
+
+## Dependencies
+
+* [peterkahl\charset-from-string](https://github.com/peterkahl/Charset-From-String)
+* [peterkahl\gender-hanzi](https://github.com/peterkahl/Gender-Hanzi)
+* [peterkahl\gender-latin](https://github.com/peterkahl/Gender-Latin)
+* [peterkahl\gender-cyrillic](https://github.com/peterkahl/Gender-Cyrillic)
+
+## Usage
+
+```php
+use peterkahl\GenderUniversal\GenderUniversal;
+
+$gendObj = new GenderUniversal;
+
+#-------------------------------
+# French (Latin) name
+$gendObj->firstName = 'Gaétan';
+$gendObj->lastName  = '';         # Surname is irrelevant (in this case)
+$gendObj->country   = 'FR';       # Country code may be helpful
+
+echo $gendObj->getGender(); # M
+
+#-------------------------------
+# Russian (Cyrillic) name
+$gendObj->firstName = 'Алла';
+$gendObj->lastName  = 'Пугачёва'; # Surname may be helpful
+$gendObj->country   = '';
+
+echo $gendObj->getGender(); # F
+
+#-------------------------------
+# Chinese (Hanzi) name
+$gendObj->firstName = '澤東';      # Make sure this is only given name (not surname)
+$gendObj->lastName  = '';         # Surname is irrelevant (in this case)
+$gendObj->country   = '';         # Country code is irrelevant (in this case)
+
+echo $gendObj->getGender(); # M
+
+#-------------------------------
+# Japanese (Kanji) name
+$gendObj->firstName = '喜孝';      # Make sure this is only given name (not surname)
+$gendObj->lastName  = '';         # Surname is irrelevant (in this case)
+$gendObj->country   = '';         # Country code is irrelevant (in this case)
+
+echo $gendObj->getGender(); # M
+```
---
+++ src/GenderUniversal.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Gender Universal
+ * Detects gender from person's name, capable to analyse strings in
+ * these scripts: Latin, Cyrillic, Hanzi (Kanji).
+ *
+ * @version    0.1 (2017-05-14 00:03:00 GMT)
+ * @author     Peter Kahl <peter.kahl@colossalmind.com>
+ * @since      2017-01-27
+ * @license    Apache License, Version 2.0
+ *
+ * Copyright 2017 Peter Kahl <peter.kahl@colossalmind.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      <http://www.apache.org/licenses/LICENSE-2.0>
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace peterkahl\GenderUniversal;
+
+use peterkahl\CharsetFromString\CharsetFromString;
+use peterkahl\GenderHanzi\GenderHanzi;
+use peterkahl\GenderLatin\GenderLatin;
+use peterkahl\GenderCyrillic\GenderCyrillic;
+
+use \Gender\Gender;
+
+class GenderUniversal {
+
+  /**
+   * Country code
+   * @var string
+   */
+  public $country = '';
+
+  /**
+   * First Name: REQUIRED
+   * @var string
+   */
+  public $firstName;
+
+  /**
+   * Last Name: OPTIONAL
+   * @var string
+   */
+  public $lastName = '';
+
+  #===================================================================
+
+  public function getGender() {
+
+    $charset = CharsetFromString::getCharset($this->firstName . $this->lastName);
+
+    if ($charset == 'CJK' || $charset == 'JAPANESE') {
+      return GenderHanzi::getGender($this->firstName);
+    }
+    elseif ($charset == 'CYRILLIC') {
+      return GenderCyrillic::getGender($this->firstName, $this->lastName);
+    }
+    elseif ($charset == 'LATIN') {
+      $latGen = new GenderLatin;
+      $latGen->firstName = $this->firstName;
+      $latGen->lastName  = $this->lastName;
+      $latGen->country   = $this->country;
+      return $latGen->getGender();
+    }
+
+    # Other charsets not configured yet.
+    return 'N';
+  }
+
+  #===================================================================
+}
